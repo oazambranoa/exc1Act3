1f502ffec682324dbe7d545be3e9f8408c3f3c2c
oazambranoa <oazambranoa@unal.edu.co>
2023-09-18 10:43:37 -0500

    Finalizacion de los ejercicios

---
+++ src/main/java/com/mycompany/exc1act3/Exc1Act3.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exc1act3;
+
+/**
+ *
+ * @author omarz
+ */
+/*Enunciado:
+18. Se tiene la siguiente información de un empleado:
+ · código del empleado,
+ · nombres,
+ · número de horas trabajadas al mes,
+ · valor hora trabajada,
+ · porcentaje de retención en la fuente. 
+ Haga un algoritmo que muestre: código, nombres, salario bruto y salario neto. 
+*/
+import java.util.Scanner;
+
+public class Exc1Act3 {
+
+    public static void main(String[] args) {
+        
+        e1A3 form = new e1A3();
+        
+        form.setVisible(true);
+        
+    }
+}
